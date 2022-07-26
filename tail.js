b3a766dda5b5a718810087cf47d3ab3d038fdde5
JasonDisj <jasondisj@gmail.com>
2022-07-26 04:46:54 +0000

    Modified

--- tail.js
+++ tail.js
@@ -1,3 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed ${actual} === ${expected}`)
+  } else {
+    console.log(`Assertion Failed ${actual} !== ${expected}`)
+  }
+};
+
 const tail = function(arr) {
   if (arr.length === 0) {
     console.log("Array is empty");
@@ -8,5 +16,6 @@ const tail = function(arr) {
     return arr;
   }
 };
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]);
