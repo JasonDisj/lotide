a9b7b9817a3ef8ba285be5c26fdc4a96a64e4fc1
JasonDisj <jasondisj@gmail.com>
2022-07-26 03:59:30 +0000

    Modified

--- head.js
+++ head.js
@@ -1,10 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed ${actual} === ${expected}`)
+  } else {
+    console.log(`Assertion Failed ${actual} !== ${expected}`)
+  }
+};
+
 const head = function(arr) {
   if (arr.length === 0) {
-    console.log("Array is empty");
+    console.log("undefined");
   } else {
     return arr[0];
   }
 };
 
-console.assert(head([5,6,7]), 5);
-console.assert(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
