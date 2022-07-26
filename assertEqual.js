40bfdeead613ce17c4dc62e73a8183e99957a075
JasonDisj <jasondisj@gmail.com>
2022-07-26 03:40:09 +0000

    Modified

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,10 @@
 const assertEqual = function(actual, expected) {
-  return actual * expected;
+  if (actual === expected) {
+    console.log(`Assertion Passed ${actual} === ${expected}`)
+  } else {
+    console.log(`Assertion Failed ${actual} !== ${expected}`)
+  }
 };
 
-console.assert(assertEqual(1, 1) === 3);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
