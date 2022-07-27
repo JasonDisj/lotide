72c18c841ad4b776480d71ecab37ed6685f673ab
JasonDisj <jasondisj@gmail.com>
2022-07-27 00:41:26 +0000

    Equal Arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log("Passed");
+  } else {
+    console.log("Failed");
+  }
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } 
+    }
+    return true;
+}
+
+assertArraysEqual([1, 2, 3], [1, 6, 9]);
---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Passed");
+  } else {
+    console.log("Failed");
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } 
+    }
+    return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 6]), true);
