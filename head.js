fbbedcc871ca6a40fda49b953f5ecfb41f514b95
JasonDisj <jasondisj@gmail.com>
2022-08-01 19:14:41 +0000

    Modularizing version

--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed ${actual} !== ${expected}`)
-  }
-};
-
 const head = function(arr) {
   if (arr.length === 0) {
     console.log("undefined");
@@ -14,5 +6,4 @@ const head = function(arr) {
   }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
