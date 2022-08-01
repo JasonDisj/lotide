c60ed94cebbf56d583a82a30d33fb832e024f610
JasonDisj <jasondisj@gmail.com>
2022-08-01 23:54:27 +0000

    export & require version

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
@@ -5,17 +7,19 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 }
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } 
-    }
-    return true;
-}
+
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] !== arr2[i]) {
+//         return false;
+//       } 
+//     }
+//     return true;
+// }
+
 const words = ["ground", "control", "to", "major", "tom"];
 const map = function(array, callback) {
   const results = [];
@@ -28,3 +32,5 @@ const map = function(array, callback) {
 const results1 = map(words, word => word[0]);
 
 assertArraysEqual(results1, [ "hello", "world", "lighthouse" ]);
+
+module.exports = map;
