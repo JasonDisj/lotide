a0448a64c2d9a330543d088f8bfd2562f8191640
JasonDisj <jasondisj@gmail.com>
2022-08-01 23:51:46 +0000

    export & require version

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed ${actual} !== ${expected}`)
-  }
-};
+const eqArrays = require('./eqArrays');
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed ${actual} === ${expected}`)
+//   } else {
+//     console.log(`Assertion Failed ${actual} !== ${expected}`)
+//   }
+// };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } 
-    }
-    return true;
-};
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] !== arr2[i]) {
+//         return false;
+//       } 
+//     }
+//     return true;
+// };
 
 const eqObjects = function(object1, object2) {
   let keyArr1 = Object.keys(object1); // => all keys in obj1 as array
@@ -41,3 +42,5 @@ console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
+
+module.exports = eqObjects;
