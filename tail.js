6d6ce1da479cb7a4c71788af297eae82ae2bfb0b
JasonDisj <jasondisj@gmail.com>
2022-08-01 19:38:17 +0000

    Modularizing version

--- tail.js
+++ tail.js
@@ -1,21 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed ${actual} !== ${expected}`)
-  }
-};
-
 const tail = function(arr) {
+  let result = [];
   if (arr.length === 0) {
     console.log("Array is empty");
   } else if (arr.length === 1) {
     console.log("Tail equals to head");
   } else if (arr.length > 1) {
-    let removedElement = arr.shift();
-    return arr;
+    for (let i = 1; i < arr.length; i++) {
+      result.push(arr[i]);
+    }
+    return result;
   }
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+module.exports = tail;
