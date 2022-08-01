a17f58c5440bc7f09d15c02fa86bd8bda9461e5f
JasonDisj <jasondisj@gmail.com>
2022-08-01 23:50:51 +0000

    export & require version

--- countOnly.js
+++ countOnly.js
@@ -1,15 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed ${actual} !== ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual');
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed ${actual} === ${expected}`)
+//   } else {
+//     console.log(`Assertion Failed ${actual} !== ${expected}`)
+//   }
+// };
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-    console.log(item);
     if (itemsToCount[item]) { 
       if (results[item]) {
         results[item] += 1;
@@ -39,3 +40,5 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+
+module.exports = countOnly;
