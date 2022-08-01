dedc1675e840499ecdfde997903ee1fa19cd6e87
JasonDisj <jasondisj@gmail.com>
2022-08-01 23:52:57 +0000

    export & require version

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed ${actual} === ${expected}`)
-  } else {
-    console.log(`Assertion Failed ${actual} !== ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual');
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed ${actual} === ${expected}`)
+//   } else {
+//     console.log(`Assertion Failed ${actual} !== ${expected}`)
+//   }
+// };
 
 //const findKeyByValue = function(object, value) {
   //let odjKeysArr = Object.keys(object);
@@ -36,4 +38,6 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "comedy");
+
+module.exports = findKeyByValue;
