7a610c9b2b10479eed938e3c73c73467364e5882
JasonDisj <jasondisj@gmail.com>
2022-08-01 21:09:32 +0000

    mocha test combined with tail.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Lighthouse");
+describe("#tail", () => {
+  it('return ["lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  }),
+
+  it('return [2, 3, 4] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4])
+  })
+})
