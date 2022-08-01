214dfbd298221b406e03d0b5a0f2eaec97f3fed7
JasonDisj <jasondisj@gmail.com>
2022-08-01 23:49:19 +0000

    export & require version

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,5 @@
+const eqObjects = require('./eqObjects');
+
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
@@ -7,21 +9,23 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-const eqObjects = function(object1, object2) {
-  let keyArr1 = Object.keys(object1);
-  let keyArr2 = Object.keys(object2);
-  if (keyArr1.length !== keyArr2.length) {
-    return false;
-  }
-    for (let key of keyArr1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        return eqArrays(object1[key], object2[key]);
-      }
-        else if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-    return true;
-};
+// const eqObjects = function(object1, object2) {
+//   let keyArr1 = Object.keys(object1);
+//   let keyArr2 = Object.keys(object2);
+//   if (keyArr1.length !== keyArr2.length) {
+//     return false;
+//   }
+//     for (let key of keyArr1) {
+//       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+//         return eqArrays(object1[key], object2[key]);
+//       }
+//         else if (object1[key] !== object2[key]) {
+//         return false;
+//       }
+//     }
+//     return true;
+// };
 
 assertObjectsEqual({ a: '1', b: 2, c: '3' }, { b: 2, a: '1', c: '3' });
+
+module.exports = assertObjectsEqual;
